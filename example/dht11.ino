cc41505425d2f617b82369cc22b1fa8b67885b8e
jcjones94 <jcjones9488@gmail.com>
2017-12-02 01:39:18 -0500

    Add files via upload

---
+++ example/dht11.ino
@@ -0,0 +1,18 @@
+#include "dht11.h"
+
+dht dht(2);
+uint8_t *data;
+void setup() {
+  Serial.begin(9600);
+  
+  dht.begin();
+}
+
+void loop() {
+  if(dht.read(0)){
+    data = dht.getData();
+    Serial.print(data[0]);
+    Serial.print(" ");
+    Serial.println(data[2]);
+  }
+}
